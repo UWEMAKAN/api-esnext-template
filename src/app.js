2ce08a78bdbe1ed8c9be5ab073fca259501ee56a
Uwem Akan Nkereuwem <uwemakan12@gmail.com>
2020-06-09 17:06:26 +0100

    setup and config

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import http from 'http';
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
+import logger from './common/winston';
+import config from './config/appConfig';
+
+const app = express();
+config(app);
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => res.send('Up and Running'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.all('*', (req, res) => {
+  res.status(404);
+  return res.send('Ooops! Not Found!!!');
+});
+
+export const server = http.createServer(app);
+server.listen(PORT, () => {
+  logger.debug(`Listening on port ${PORT}`);
+});
+
+export default app;
